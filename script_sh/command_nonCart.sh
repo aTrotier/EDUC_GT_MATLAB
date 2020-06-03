da5b77ca67596345138480b14e0c7c752e205208
aTrotier <a.trotier@gmail.com>
2020-06-03 08:27:11 +0000

    add nonCart Reco

---
+++ script_sh/command_nonCart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gadgetron_ismrmrd_client -f /home/atrotier/GITHUB/EDUC_GT_MATLAB/data/spiral_2D_ssfp.h5 -o /home/atrotier/GITHUB/EDUC_GT_MATLAB/data/out/out_spiral_2D.h5 -C /home/atrotier/GITHUB/EDUC_GT_MATLAB/config/demo_2_nonCart.xml
+
+
